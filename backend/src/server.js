4ac7de8be408c2ddf6eb8e63237f528cc89e9108
Gauri Mehrotra <gaurimehrotra18@gmail.com>
2025-11-15 22:19:36 +0530

    cors typo

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,16 +8,19 @@ const port = process.env.PORT || 3000;
 const app=express();
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://study-mate-dvklf0xoj-gauris-projects-e00c6357.vercel.app/'],
+  origin: ['http://localhost:5173','https://study-mate-dvklf0xoj-gauris-projects-e00c6357.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
-
+app.options("*", cors());
 app.use(cookie())
 app.use(express.json())
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'Server is running', port: port })
 })
+
+//checking for error
 
 app.use('/api/auth', router)
 app.listen(port, ()=>{
